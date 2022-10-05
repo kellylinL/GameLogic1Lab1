859d5ffd5aa4005561f999bbbd0d6ae705bf09d3
Arthur Senna <adesenna@my.centennialcollege.ca>
2022-10-05 14:58:32 -0400

    Add QuestItemController

---
+++ Assets/Scripts/QuestItemController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class QuestItemController : MonoBehaviour
+{
+    public ItemSO itemData;
+    public bool isPickup = false;
+    public QuestTransactor _questTransactor;
+    void Start()
+    {
+        _questTransactor = FindObjectOfType<QuestTransactor>();
+    }
+    public void ClickItem()
+    {
+        if (isPickup)
+        {
+            AddToInventory();
+        }
+        else
+        {
+            RemoveItemFromInventory();
+        }
+    }
+
+    private void AddToInventory()
+    {
+        _questTransactor.AddQuestItem(itemData);
+    }
+    private void RemoveItemFromInventory()
+    {
+        int itemIndex = transform.GetSiblingIndex();
+        _questTransactor.RemoveQuestItem(itemIndex);
+        Destroy(gameObject);
+    }
+
+}
